543ff08c4338acc64aa59e262415486b51f9a87c
Scott Sum <scottsum@outlook.com>
2021-05-06 15:17:25 -0400

    more responsive edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: theme => ({
-        'mountain': "url('img/mountain.jpg')",
-        'teal-mountain': "url('img/teal_mountain.png')"
-      })
+      backgroundImage: (theme) => ({
+        "mountain": "url('img/mountain.jpg')",
+        "teal-mountain": "url('img/teal_mountain.png')",
+        "honeycomb": "url('img/honeycomb.jpg')"
+      }),
+    },
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
